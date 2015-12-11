1076c99f7d2a04129eb883ec3729177912a96600
tuskeb <tuskebganz@gmail.com>
2015-12-11 15:55:16 +0100

    Initial commit

---
+++ desktop/src/com/csanydroid/foodfight/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.csanydroid.foodfight.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.csanydroid.foodfight.FoodFightGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FoodFightGame(), config);
+	}
+}
